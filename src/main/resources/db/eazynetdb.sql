bfaf806b40567d0d81d80c925424b390840d4e10
Lsatin <lsatin_mail@163.com>
2021-02-20 13:51:41 +0800

    更新表结构

--- src/main/resources/db/eazynetdb.sql
+++ src/main/resources/db/eazynetdb.sql
@@ -1,38 +1,61 @@
-/*
- Navicat Premium Data Transfer
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: eazynetdb
+-- ------------------------------------------------------
+-- Server version 8.0.16
 
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 80017
- Source Host           : localhost:3306
- Source Schema         : eazynetdb
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
- Target Server Type    : MySQL
- Target Server Version : 80017
- File Encoding         : 65001
+--
+-- Table structure for table `user_info`
+--
 
- Date: 24/08/2019 01:07:48
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for user_info
--- ----------------------------
 DROP TABLE IF EXISTS `user_info`;
-CREATE TABLE `user_info`  (
-  `id` varchar(32) CHARACTER SET utf8 COLLATE utf8_estonian_ci NOT NULL COMMENT '主键id',
-  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_estonian_ci NULL DEFAULT NULL COMMENT '名称',
-  `sex` int(1) NULL DEFAULT NULL COMMENT '性别',
-  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_estonian_ci NULL DEFAULT NULL COMMENT '邮箱',
-  `address` varchar(50) CHARACTER SET utf8 COLLATE utf8_estonian_ci NULL DEFAULT NULL COMMENT '地址',
-  `mobile` varchar(50) CHARACTER SET utf8 COLLATE utf8_estonian_ci NULL DEFAULT NULL COMMENT '手机号',
-  `create_date` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `create_user` varchar(20) CHARACTER SET utf8 COLLATE utf8_estonian_ci NULL DEFAULT NULL COMMENT '创建人',
-  `modify_date` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
-  `modify_user` varchar(20) CHARACTER SET utf8 COLLATE utf8_estonian_ci NULL DEFAULT NULL COMMENT '修改人',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_estonian_ci ROW_FORMAT = Dynamic;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_estonian_ci DEFAULT NULL COMMENT '名称',
+  `nick_name` varchar(45) COLLATE utf8_estonian_ci DEFAULT NULL,
+  `sex` int(1) DEFAULT NULL COMMENT '性别',
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_estonian_ci DEFAULT NULL COMMENT '邮箱',
+  `address` varchar(50) CHARACTER SET utf8 COLLATE utf8_estonian_ci DEFAULT NULL COMMENT '地址',
+  `phone` varchar(50) CHARACTER SET utf8 COLLATE utf8_estonian_ci DEFAULT NULL COMMENT '手机号',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `create_user` varchar(20) CHARACTER SET utf8 COLLATE utf8_estonian_ci DEFAULT NULL COMMENT '创建人',
+  `modify_date` datetime DEFAULT NULL COMMENT '修改时间',
+  `modify_user` varchar(20) CHARACTER SET utf8 COLLATE utf8_estonian_ci DEFAULT NULL COMMENT '修改人',
+  PRIMARY KEY (`id`),
+  KEY `MOBILE_INDEX` (`phone`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_estonian_ci ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SET FOREIGN_KEY_CHECKS = 1;
+--
+-- Dumping data for table `user_info`
+--
+
+LOCK TABLES `user_info` WRITE;
+/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
+INSERT INTO `user_info` VALUES (1,'admin','系统管理员',0,NULL,NULL,NULL,'2021-02-20 13:48:16',NULL,'2021-02-20 13:48:16',NULL);
+/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-20 13:50:40
